b2a47bc7557427a96564b5e8b5a031d7dbc3b7da
Scott Ungchusri <animoogle504@gmail.com>
2018-05-13 00:18:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string){
+  return string.toUpperCase();
+}
+function whisper(string){
+  return string.toLowerCase();
+}
+function logShout(){
+  return console.log();
+}
+function logWhisper(){
+  return console.log().toLowerCase();
+}
+function sayHiToGrandma(string){
+  if (string.toLowerCase() === string){
+    return "I can't hear you!";
+  }
+  if (string.toUpperCase() === string){
+    return "YES INDEED!";
+  }
+}
