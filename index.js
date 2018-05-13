a1f77ec1b73b0e8f175c10de69f3087f27b78aba
Scott Ungchusri <animoogle504@gmail.com>
2018-05-13 01:00:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ function logWhisper(){
 function sayHiToGrandma(string){
   var response = "";//empty response variable
   
-  if (whisper(string) === string){
+  if (string.toLowerCase() === string){
     response = "I can't hear you!";
   } // if she can't hear you she can't reply to anything.
   
-  else if (shout(string) === string){
+  else if (string.toUpperCase() === string){
     response = "YES INDEED!";
   }
   
-  if (whisper(string) == "i love you, grandma."){
-      response = "I love you, too."; 
+  if (string.toLowerCase() === "i love you, grandma."){
+    response = "I love you, too."; 
   } // if she can hear you she replies YES INDEED, additionall if string is "I love you, Grandma"
     // it will return "I love you too"
     
